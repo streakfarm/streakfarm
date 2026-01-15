ec9d9fb8ef1abccef2b1b99a904c0b8a897ef09b
streakfarm <streakfarm@proton.me>
2026-01-15 08:15:22 +0530

    Update streaks.ts

--- packages/shared/constants/streaks.ts
+++ packages/shared/constants/streaks.ts
@@ -1,22 +1,3 @@
-export const STREAK_CONFIG = {
-  // Timezone
-  STREAK_RESET_HOUR: 0, // Midnight UTC
-  GRACE_PERIOD_HOURS: 4,
-
-  // Milestones
-  MILESTONES: [7, 14, 30, 50, 100, 200, 365],
-
-  // Rewards
-  MILESTONE_REWARDS: {
-    7: 1000,
-    14: 2500,
-    30: 5000,
-    50: 10000,
-    100: 25000,
-    200: 50000,
-    365: 100000,
-  },
-
-  // Box Rewards
-  MILESTONE_BOX_REWARDS: [7, 30, 100, 365],
-} as const;
+export const STREAK_GRACE_PERIOD_HOURS = 3;
+export const STREAK_RESET_HOUR_UTC = 0;
+export const MAX_STREAK_FREEZE_ITEMS = 3;
