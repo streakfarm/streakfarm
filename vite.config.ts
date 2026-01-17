c5fda8bea7a70d9fda12dff535fbb08ca476c845
streakfarm <streakfarm@proton.me>
2026-01-17 07:43:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";  // -swc hata do
+import react from "@vitejs/plugin-react-swc";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
