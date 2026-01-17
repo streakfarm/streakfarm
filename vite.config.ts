6ab94448542c3ec9dc1edfbfa30fee1fdf98a8d5
streakfarm <streakfarm@proton.me>
2026-01-17 07:06:28 +0530

    Update and rename vite.config.js to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/", // 👈 YE LINE CRITICAL HAI
+  base: "/",          // 🔥 NETLIFY FIX
   plugins: [react()],
 });
