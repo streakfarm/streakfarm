340014abd27db3ee1ba429be231005246baee7f4
streakfarm <streakfarm@proton.me>
2026-01-17 06:54:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // 👈 YE LINE CRITICAL HAI
   plugins: [react()],
 });
